1d5fa13c0240b31ec179b1da4fa37db773cabffa
bjarne <bjarnewilhelmhaugen@hotmail.com>
2024-08-13 18:19:40 +0200

    make it better

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Create an Express app
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, MongoDB!');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
