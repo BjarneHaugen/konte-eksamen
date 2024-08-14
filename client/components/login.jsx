094ed6b0fb38d1890c59493150ba2203c54db182
bjarne <bjarnewilhelmhaugen@hotmail.com>
2024-08-14 16:40:19 +0200

    fix stuff

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -15,39 +15,34 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add your login logic here
-    if (username === "correctUsername" && password === "correctPassword") {
-      // Logic for successful login
-    } else {
-      // Logic for unsuccessful login
-    }
   };
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
 };
+
+return (
+  <div>
+    <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+);
 
 export default Login;
